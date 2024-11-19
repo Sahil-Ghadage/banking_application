d3b2372dbb431e1519fb89039e5a5e9365ad64cf
SAHIL_G <sahilghadage5@gmail.com>
2024-11-20 00:56:20 +0530

    Basic CRUD Completed - Added Controller and Exception Handling

--- accounts/src/main/java/com/sahilg/banking/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/sahilg/banking/accounts/controller/AccountsController.java
@@ -1,10 +1,63 @@
 package com.sahilg.banking.accounts.controller;
 
+import com.sahilg.banking.accounts.constant.AccountConstants;
+import com.sahilg.banking.accounts.dto.CustomerDTO;
+import com.sahilg.banking.accounts.dto.ResponseDTO;
+import com.sahilg.banking.accounts.service.I_AccountsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
+
+    @Autowired
+    private I_AccountsService iAccountsService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> createAccount(@RequestBody CustomerDTO customerDto){
+        iAccountsService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDTO(AccountConstants.STATUS_201.getCode(), AccountConstants.STATUS_201.getMessage()));
+
+    }
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDTO> fetchAccountDetails(@RequestParam String mobileNumber) {
+        CustomerDTO customerDto = iAccountsService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDTO> updateAccountDetails( @RequestBody CustomerDTO customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDTO(AccountConstants.STATUS_200.getCode(), AccountConstants.STATUS_200.getMessage()));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDTO(AccountConstants.STATUS_417_UPDATE.getCode(), AccountConstants.STATUS_417_UPDATE.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDTO> deleteAccountDetails(@RequestParam String mobileNumber) {
+        boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDTO(AccountConstants.STATUS_200.getCode(), AccountConstants.STATUS_200.getMessage()));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDTO(AccountConstants.STATUS_417_DELETE.getCode(), AccountConstants.STATUS_417_DELETE.getMessage()));
+        }
+    }
 
 
 }
