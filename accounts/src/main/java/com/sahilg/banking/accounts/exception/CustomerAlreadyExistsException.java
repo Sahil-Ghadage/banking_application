cd0dde3bf39950bb2098e8685c992dfef521ad10
SAHIL_G <sahilghadage5@gmail.com>
2024-11-23 13:22:55 +0530

    Adding cards microservice

--- accounts/src/main/java/com/sahilg/banking/accounts/exception/CustomerAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.sahilg.banking.accounts.exception;
-
-public class CustomerAlreadyExistsException extends RuntimeException{
-    public CustomerAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- cards/src/test/java/com/sahilg/banking/cards/CardsApplicationTests.java
+++ cards/src/test/java/com/sahilg/banking/cards/CardsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sahilg.banking.accounts;
+package com.sahilg.banking.cards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AccountsApplicationTests {
+class CardsApplicationTests {
 
 	@Test
 	void contextLoads() {
